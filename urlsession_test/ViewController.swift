fba87aa928389a9dcf8c868596ec9603a712ed46
lucas Heo <hsj02036@gmail.com>
2021-02-21 23:57:42 +0900

    outlet connect

--- urlsession_test/ViewController.swift
+++ urlsession_test/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var resultData: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func networkRun(_ sender: Any) {
+    }
 }
-

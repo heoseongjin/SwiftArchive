5525d778331dc37c9039621037b9b033933dd45b
lucas Heo <hsj02036@gmail.com>
2021-03-07 23:58:09 +0900

    initial

--- radioButton/ViewController.swift
+++ radioButton/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import DLRadioButton
 
 class ViewController: UIViewController {
 
+    @IBOutlet var radioButton1: DLRadioButton!
+    @IBOutlet var radioButton2: DLRadioButton!
+    @IBOutlet var radioButton3: DLRadioButton!
+    @IBOutlet var radioButton4: DLRadioButton!
+    @IBOutlet var radioButton5: DLRadioButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        radioButton1.setTitleColor(UIColor.blue, for: .selected)
+        radioButton2.setTitleColor(UIColor.blue, for: .selected)
+        radioButton3.setTitleColor(UIColor.blue, for: .selected)
+        radioButton4.setTitleColor(UIColor.blue, for: .selected)
+        radioButton5.setTitleColor(UIColor.blue, for: .selected)
+        
+        radioButton1.addTarget(self, action: #selector(btnTouch(_:)), for: .touchUpInside)
+        radioButton2.addTarget(self, action: #selector(btnTouch(_:)), for: .touchUpInside)
+        radioButton3.addTarget(self, action: #selector(btnTouch(_:)), for: .touchUpInside)
+        radioButton4.addTarget(self, action: #selector(btnTouch(_:)), for: .touchUpInside)
+        radioButton5.addTarget(self, action: #selector(btnTouch(_:)), for: .touchUpInside)
     }
-
-
+    
+    @objc func btnTouch(_ sender: DLRadioButton) {
+        print(sender.currentTitle!)
+    }
 }
 
